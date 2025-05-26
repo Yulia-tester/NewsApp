c774c2778d7b2b6a04ca575bec572d45c85d82fe
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-05-26 14:08:35 -0500

    1. Added UICollectionViewCell and GUI var for cells 2. Added elements to view and added constraints 3. Registered the cell and implement UICollectionViewDataSource 4. Added image 5. Increased the cell size 6. Adjusted the size of the gaps between cells

---
+++ NewsApp/Business/BusinessViewController.swift
@@ -0,0 +1,28 @@
+//
+//  BusinessViewController.swift
+//  NewsApp
+//
+//  Created by Юлия Дегтярева on 2025-05-11.
+//
+
+import Foundation
+import UIKit
+
+final class BusinessViewController: UIViewController {
+    
+    // MARK: - GUI Variables
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - Private methods
+}
