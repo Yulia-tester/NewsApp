c332349d1dba84630b2fb4ffea18bf9d7c148e6b
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-05-26 14:08:54 -0500

    no message

---
+++ NewsApp/TabBarController.swift
@@ -0,0 +1,48 @@
+//
+//  TabBarController.swift
+//  NewsApp
+//
+//  Created by Юлия Дегтярева on 2025-05-11.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.tintColor = .black
+        setupViewControllers()
+    }
+    
+    private func setupViewControllers() {
+        viewControllers = [
+            setupNavigationController(rootViewController: GeneralViewController(),
+                                      title: "General",
+                                      imageName:  "newspaper"),
+            setupNavigationController(rootViewController: BusinessViewController(),
+                                      title: "Business",
+                                      imageName: "briefcase"),
+            setupNavigationController(rootViewController: TechnologyViewController(),
+                                      title: "Technology",
+                                      imageName: "gyroscope")
+            
+        ]
+    }
+    
+    private func setupNavigationController(rootViewController: UIViewController,
+                                           title: String,
+                                           imageName: String) -> UINavigationController
+    {
+        
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        
+        navigationController.tabBarItem.title = title
+        navigationController.tabBarItem.image = UIImage(systemName: imageName)
+        rootViewController.navigationItem.title = title
+        navigationController.navigationBar.prefersLargeTitles = true
+        
+        return navigationController
+    }
+}
