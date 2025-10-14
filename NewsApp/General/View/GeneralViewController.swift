2f16e1e279017be833abccc317cd7c2019b19a51
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-10-14 11:14:56 -0500

    Display text and other data on the news viewing screen

--- NewsApp/General/View/GeneralViewController.swift
+++ NewsApp/General/View/GeneralViewController.swift
@@ -121,7 +121,9 @@ extension GeneralViewController: UICollectionViewDataSource {
 extension GeneralViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView,
                         didSelectItemAt indexPath: IndexPath) {
-        navigationController?.pushViewController(NewsViewController(), animated: true)
+        let article = viewModel.getArticle(for: indexPath.row)
+        navigationController?.pushViewController(NewsViewController(viewModel: NewsViewModel(aeticle: article)),
+                                                 animated: true)
     }
 }
 
